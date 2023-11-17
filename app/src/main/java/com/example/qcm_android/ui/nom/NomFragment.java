00c5baa53b9daed1d411b308cabb59cb5c2b6b5a
Ronan PEYREL <archib71@gmail.com>
2023-11-17 16:38:42 +0100

    ajout du nom

--- app/src/main/java/com/example/qcm_android/ui/nom/NomFragment.java
+++ app/src/main/java/com/example/qcm_android/ui/nom/NomFragment.java
@@ -1,5 +1,6 @@
 package com.example.qcm_android.ui.nom;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +10,13 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.qcm_android.databinding.FragmentNomBinding;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class NomFragment extends Fragment{
         private FragmentNomBinding binding;
@@ -28,5 +33,26 @@ public class NomFragment extends Fragment{
         public void onDestroyView() {
             super.onDestroyView();
             binding = null;
+        }
+
+        /**
+         * Enregistre le nom de l'utilisateur dans un fichier
+         * @param nom nom de l'utilisateur
+         */
+        private void nameRegister(String nom) throws IOException {
+            String filename = "userName.txt";
+            // test if file exists
+            File file = new File(requireActivity().getFilesDir(), filename);
+            Context context = getActivity();
+            assert context != null;
+            if (file.exists()) {
+                FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
+                fos.write(nom.getBytes());
+            }else{
+                //create file
+                file.createNewFile();
+                FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
+                fos.write(nom.getBytes());
+            }
         }
 }
